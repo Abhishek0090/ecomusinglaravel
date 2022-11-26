2c37d9a76efe0f00ec2e3f845c57220b73c62b17
Abhishek0090 <palabhishek411@gmail.com>
2022-11-26 13:15:54 +0530

    first commit

---
+++ ecom-dashboard/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">  
+        <h1>Ecom Dashboard</h1> 
+        <button>bruh</button>
+        <Button>BROOO</Button>
+    </div>
+  );
+}
+
+export default App;
